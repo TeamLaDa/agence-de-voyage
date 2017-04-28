25f740514d117295b88360f8062672dbf59bd15c
Unknown <laurent.esteve@soprasteria.com>
2017-04-28 16:22:03 +0200

    Implementation des methodes adresseDAO et passagerDAO

--- src/main/java/Interface/AdresseDAO.java
+++ src/main/java/Interface/AdresseDAO.java
@@ -7,6 +7,8 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import edu.formation.agence.Adresse;
@@ -29,11 +31,23 @@ public class AdresseDAO implements ILectureFichiers<Adresse>{
 	@Override
 	public List<Adresse> findAll() {
 		// TODO Auto-generated method stub
-		return null;
+		// TODO Auto-generated method stub
+		String op;
+		
+		op = this.buffer_in.readLine();
+		List<Adresse> adresses = new ArrayList<>();
+		
+		while((op = this.buffer_in.readLine()) != null){
+			String tab_op[] = op.split(";");
+			Adresse adresse = new Adresse(tab_op[1], tab_op[2], tab_op[3], tab_op[4]);
+			adresses.add(adresse);
+		}
+		this.buffer_in.close();
+		return adresses;
 	}
 
 	@Override
-	public Adresse findById(Integer id) {
+	public Adresse findById(Integer id) throws IOException {
 		// TODO Auto-generated method stub
 		String op;
 		op = this.buffer_in.readLine();
@@ -41,18 +55,17 @@ public class AdresseDAO implements ILectureFichiers<Adresse>{
 		
 		while((op = this.buffer_in.readLine()) != null){
 			String tab_op[] = op.split(";");
-			int id_passager = Integer.parseInt(tab_op[0]);
-			
-			if(id_passager == id){
-				int id_adresse = Integer.parseInt(tab_op[3]);
-				passager.setAdresse(adresseDAO.findById(id_adresse));
-				passager.setNom(tab_op[1]);
-				passager.setPrenom(tab_op[2]);
+			int id_adresse = Integer.parseInt(tab_op[0]);			
+			if(id_adresse == id){
+				adresse.setAdresse(tab_op[1]);
+				adresse.setCodePostal(tab_op[2]);
+				adresse.setVille(tab_op[3]);
+				adresse.setPays(tab_op[4]);
 			}
 
 		}
 		this.buffer_in.close();
-		return passager;
+		return adresse;
 	}
 
 }
