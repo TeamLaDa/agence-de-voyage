dcc59709fc81dffdae2fd5f4bc40f57e613d53b1
Damien Lamalle <damien.lamalle@soprasteria.com>
2017-04-28 15:21:47 +0200

    Ajout des fichiers d'entrée + divers classes
    Les classes sont adresses et passagers

--- src/main/java/edu/formation/agence/Adresse.java
+++ src/main/java/edu/formation/agence/Adresse.java
@@ -8,5 +8,88 @@ package edu.formation.agence;
  *
  */
 public class Adresse {
+	
+	public String adresse;
+	public String codePostal;
+	public String ville;
+	public String pays;
+	
+	
+	/**
+	 * @param adresse
+	 * @param codePostal
+	 * @param ville
+	 * @param pays
+	 */
+	public Adresse(String adresse, String codePostal, String ville, String pays) {
+		this.adresse = adresse;
+		this.codePostal = codePostal;
+		this.ville = ville;
+		this.pays = pays;
+	}
+
+
+	/**
+	 * @return the adresse
+	 */
+	public String getAdresse() {
+		return adresse;
+	}
+
+
+	/**
+	 * @param adresse the adresse to set
+	 */
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+
+
+	/**
+	 * @return the codePostal
+	 */
+	public String getCodePostal() {
+		return codePostal;
+	}
+
+
+	/**
+	 * @param codePostal the codePostal to set
+	 */
+	public void setCodePostal(String codePostal) {
+		this.codePostal = codePostal;
+	}
+
+
+	/**
+	 * @return the ville
+	 */
+	public String getVille() {
+		return ville;
+	}
+
+
+	/**
+	 * @param ville the ville to set
+	 */
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+
+
+	/**
+	 * @return the pays
+	 */
+	public String getPays() {
+		return pays;
+	}
+
+
+	/**
+	 * @param pays the pays to set
+	 */
+	public void setPays(String pays) {
+		this.pays = pays;
+	}
 
 }
--- src/main/java/edu/formation/agence/Passager.java
+++ src/main/java/edu/formation/agence/Passager.java
@@ -3,13 +3,116 @@
  */
 package edu.formation.agence;
 
+import java.util.List;
+
 /**
  * @author ajc
  *
  */
 public class Passager {
-	
-	String nom;
-	
+
+	private Integer id;
+	private String nom;
+	private String prenom;
+	private Adresse adresse; // code permettant l'association entre la classe
+								// Adresse et Passager
+	private List<Reservation> reservationList;
+
+	public static Integer nbPassagers = 0;
+
+	/**
+	 * Constructeur par défaut
+	 */
+	public Passager() {
+		this.id = ++nbPassagers;
+	}
+
+	/**
+	 * Constructeur incluant le nom et le prénom
+	 * 
+	 * @param id
+	 * @param nom
+	 * @param prenom
+	 */
+	public Passager(String nom, String prenom) {
+		this.id = ++nbPassagers;
+		this.nom = nom;
+		this.prenom = prenom;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the nom
+	 */
+	public String getNom() {
+		return nom;
+	}
+
+	/**
+	 * @param nom
+	 *            the nom to set
+	 */
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	/**
+	 * @return the prenom
+	 */
+	public String getPrenom() {
+		return prenom;
+	}
+
+	/**
+	 * @param prenom
+	 *            the prenom to set
+	 */
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	/**
+	 * @return the adresse
+	 */
+	public Adresse getAdresse() {
+		return adresse;
+	}
+
+	/**
+	 * @param adresse
+	 *            the adresse to set
+	 */
+	public void setAdresse(Adresse adresse) {
+		this.adresse = adresse;
+	}
+
+	/**
+	 * @return the reservationList
+	 */
+	public List<Reservation> getReservationList() {
+		return reservationList;
+	}
+
+
+	/**
+	 * @return the nbPassagers
+	 */
+	public static Integer getNbPassagers() {
+		return nbPassagers;
+	}
 
 }
