c48ff209ac9f670067759f979720695c85113757
Unknown <laurent.esteve@soprasteria.com>
2017-04-28 11:34:02 +0200

    Ajout d'un message dans le main

--- src/main/java/edu/formation/agence/MainAgence.java
+++ src/main/java/edu/formation/agence/MainAgence.java
@@ -13,8 +13,7 @@ public class MainAgence {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("je veux une augmentation");
 	}
 
 }
