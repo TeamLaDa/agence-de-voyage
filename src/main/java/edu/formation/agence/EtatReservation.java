f2a2394deae99583de66a7e5dc8c6ff1b8d96275
Unknown <laurent.esteve@soprasteria.com>
2017-04-28 14:43:30 +0200

    Revert "Implementation de enum EtatReservation"
    This reverts commit 3b427da7fe5cd5ac0a53c70cb3d3d46ef54b45d7.

--- src/main/java/edu/formation/agence/EtatReservation.java
+++ src/main/java/edu/formation/agence/EtatReservation.java
@@ -6,20 +6,9 @@ package edu.formation.agence;
 /**
  * @author ajc
  *
- *Cet classe de type enum permet de définir deux états : annulée ou confirmée
  */
 public enum EtatReservation {
 	
-	Annulee("annulée"), 
-	Confirmee("confirmée");
-	
-	private String etat;
-	
-	private EtatReservation(String aEtat){
-		this.etat=aEtat;
-	}
+	annulee, confirmee;
 
-	public String toString(){
-		return this.etat;
-	}
 }
