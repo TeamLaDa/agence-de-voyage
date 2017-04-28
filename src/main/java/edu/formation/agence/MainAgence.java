20220d26d2bb8e5cd1c7fbd1624817b23c8542dd
Damien Lamalle <damien.lamalle@soprasteria.com>
2017-04-28 11:00:27 +0200

    Premier commit du main
    ajout du main au projet

---
+++ src/main/java/edu/formation/agence/MainAgence.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package edu.formation.agence;
+
+/**
+ * @author ajc
+ *
+ */
+public class MainAgence {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
