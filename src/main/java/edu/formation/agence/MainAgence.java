51f9cb97ecb8f62a5905f6b145f1fc7dcd2fdb95
Damien Lamalle <damien.lamalle@soprasteria.com>
2017-04-28 14:52:12 +0200

    Ajout des fichiers pour l'interface

--- src/main/java/edu/formation/agence/MainAgence.java
+++ src/main/java/edu/formation/agence/MainAgence.java
@@ -13,8 +13,9 @@ public class MainAgence {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("je veux une augmentation");
-		System.out.println("Déjà on ne dit pas \"je voudrais\" mais \"serait-il possible d'avoir\", et ensuite, va voir les RH pour ça !!!");
+		
+		
+		
 	}
 
 }
