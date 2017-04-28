ae0b39295977602ce621145b79d3b131f861bc96
Damien Lamalle <damien.lamalle@soprasteria.com>
2017-04-28 14:07:12 +0200

    Mise à jour du Main

--- src/main/java/edu/formation/agence/MainAgence.java
+++ src/main/java/edu/formation/agence/MainAgence.java
@@ -14,7 +14,7 @@ public class MainAgence {
 	 */
 	public static void main(String[] args) {
 		System.out.println("je veux une augmentation");
-		System.out.println("Va voir les RH");
+		System.out.println("Déjà on ne dit pas \"je voudrais\" mais \"serait-il possible d'avoir\", et ensuite, va voir les RH pour ça !!!");
 	}
 
 }
